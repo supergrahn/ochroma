15f9a78be64337c2f03cea30ac903782c159cf4c
Supergrahn <tom_espen@live.com>
2026-08-02 20:02:16 +0200

    Expose required MegaGeometry root flag

--- crates/vox_render/src/lib.rs
+++ crates/vox_render/src/lib.rs
@@ -124,7 +124,7 @@ pub use spectra_scene_state::SceneState;
 #[cfg(feature = "spectra-native")]
 pub use spectra_scene_state::TerrainSdfLayer;
 #[cfg(feature = "spectra-native")]
-pub use spectra_scene_state::{GpuPageDescriptor, PAGE_NONE};
+pub use spectra_scene_state::{GpuPageDescriptor, PAGE_FLAG_REQUIRED_BASE, PAGE_NONE};
 #[cfg(feature = "spectra-native")]
 pub use spectra_scene_state::{MegaGeometryExecution, MegaGeometryLayer};
 
